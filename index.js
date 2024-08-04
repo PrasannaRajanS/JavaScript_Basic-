cf17065c46c5359e1640e5d7d9e58b88a9e360fe
PrasannaRajanS <prasanna555001@gmail.com>
2024-08-04 17:32:20 +0530

    fetch:Praticed Code

--- index.js
+++ index.js
@@ -355,3 +355,347 @@ function showStars(rows) {
 }
 showStars(5)
 
+// Objects
+
+// OOP (Object-Oriented Programing)
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 2
+    },
+    isVisible: true,
+    draw: function () {
+        console.log("This is draw function");
+    }
+}
+
+circle.draw()  // We call it as a Method 
+
+// Factory Function Single defenation for draw method
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    }
+}
+
+// Two - different draw object
+const circle1 = createCircle(1)
+console.log(circle1);
+
+const circle2 = createCircle(2)
+console.log(circle2);
+
+// Camel Notation : oneTwoThree
+// Pascal Notation : OneTwoThree
+
+// Constructor Function 
+function Circle(radius) {
+    this.radius = radius   // this is a keyword reference to the object execute this piece of code
+    this.draw = function () {
+        console.log("Draw");
+
+    }
+}
+
+const circleIs = new Circle(1)  // here new will create empty JavaScript Object
+console.log(circleIs);
+
+// Dynamic Nature Of Object   // we Can add new properties, method and remove existing one
+const MyCircleIs = {
+    radius: 1
+}
+
+MyCircleIs.color = 'Red'
+MyCircleIs.draw = function () { }
+
+delete MyCircleIs.color
+delete MyCircleIs.draw
+
+console.log(MyCircleIs);
+
+new String() // '',"".``  String Literals
+new Boolean() // true false Boolean Literals
+new Number() // 1,2,3 ... Number Literals
+
+// Every Object In JavaScript has a constructor property 
+
+function Circle1(radius) {
+    this.radius = radius
+    this.draw = function () {
+        console.log("Draw");
+
+    }
+}
+
+Circle1.call({}, 1) // the curly brases represent this keyword here
+Circle1.apply({}, [1, 2, 3, 4])
+
+const circleIs1 = new Circle(1) // If we did't give new here it will reperesent global object window here
+
+// Values VS Refereance type
+
+// Primitives are copied by their value   // Objects are copied by their referance
+let firstValue = 10
+let secondValue = firstValue  // here first value and second values are independent
+firstValue = 20
+console.log(firstValue);
+console.log(secondValue);
+
+let firstObject = { value: 10 }
+let secondObject = firstObject // here object values are storing some were in memory the using it
+firstObject.value = 20
+
+console.log(firstObject);
+console.log(secondObject);
+
+// Here the two values are independent in primitive type
+let Value1 = 10
+function increase(Value1) {
+    return Value1++;
+}
+
+increase(Value1)
+console.log(Value1);
+
+let Obj = { value: 10 }
+function increase(Obj) {
+    return Obj.value++;
+}
+
+increase(Obj)
+console.log(Obj);
+
+// Enumerating Properties of an  Object
+
+const oneCircle = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+
+    }
+}
+
+for (let key in oneCircle) {
+    console.log(key, oneCircle[key]);
+}
+
+for (let key of Object.keys(oneCircle)) {
+    console.log(key);
+}
+
+for (let key of Object.entries(oneCircle)) {
+    console.log(key);
+}
+
+if ('radius' in oneCircle) console.log("Yes");
+
+// Cloning an Object
+const clonObj = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+
+    }
+}
+
+// const anotherObj = {}
+// for (let key in clonObj)
+//     anotherObj[key] = clonObj[key]
+
+// const anotherObj = Object.assign({},clonObj)
+const anotherObj = { ...clonObj }
+console.log('anotherObj', anotherObj);
+
+Math.random()
+
+const message = "  My name is prasanna  "
+console.log(message.length);
+console.log(message[0]);
+console.log(message[1]);
+console.log(message.includes('My'));
+console.log(message.includes('are'));
+console.log(message.startsWith('My'));
+console.log(message.startsWith('my'));
+console.log(message.endsWith('anna'));
+console.log(message.endsWith('name'));
+console.log(message.replace('prasanna', 'Rajan'));
+console.log(message.toLocaleLowerCase());
+console.log(message.toLocaleUpperCase());
+console.log(message.trim());
+console.log(message.trimStart());
+console.log(message.trimEnd());
+
+const address = {
+    street: "Marai Malai Adigalar Salai",
+    city: "Vadalur",
+    zipCode: 607303
+}
+
+function showAddress(address) {
+    for (let data in address)
+        console.log(data, ':', address[data]);
+}
+
+showAddress(address)
+
+function factorialObject(street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode,
+        address() {
+            console.log("Cuddalore Distc");
+        }
+    }
+}
+
+const FactObj = factorialObject("Marai Malai Adigalar Salai", "Vadalur", 607303)
+console.log(FactObj);
+
+
+function ConstFun(street, city, pinCode) {
+    this.street = street
+    this.city = city
+    this.pinCode = pinCode
+}
+
+let dataIs = new ConstFun("Anna Nagar", 'chennai', 600028)
+let dataIs1 = new ConstFun("Anna Nagar", 'chennai', 600028)
+console.log(dataIs);
+
+function areEqual(dataIs, dataIs1) {
+    return dataIs.street === dataIs1.street &&
+        dataIs.city === dataIs1.city && dataIs.pinCode === dataIs1.pinCode
+}
+
+function areSame(dataIs, dataIs1) {
+    return dataIs === dataIs1
+}
+
+console.log(areSame(dataIs, dataIs1)); // this is checking they are same object
+
+console.log(areEqual(dataIs, dataIs1)); // this is checking they are same values
+
+let post = {
+    title: 'Book',
+    body: 'about the book',
+    author: 'book author',
+    views: 10,
+    Comments: [
+        { author: 'author - 1', body: 'Book Body - 1' },
+        { author: 'author - 2', body: 'Book Body - 2' },
+    ],
+    isLive: true
+}
+
+console.log(post);
+
+
+function BookPost(title, body, author) {
+
+    this.title = title
+    this.body = body
+    this.author = author
+    this.views = 0
+    this.Comments = []
+    this.isLive = false
+}
+
+let PostOne = new BookPost('Book Name', 'Book Description', 'Book Author')
+console.log(PostOne);
+
+let priceRanges = [
+    { lable: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { lable: '$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { lable: '$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+];
+
+let restaurants = [
+    { averagePerPerson: 5 }
+]
+
+// Adding Elements 
+
+const numbers = [3, 4]
+
+numbers.push(5, 6)   // It will add in the end 
+
+numbers.unshift(1, 2) // It will add in the Start
+
+numbers.splice(0, 0, 'a', 'b', 'c', 'd') // It will add in the Middle
+
+console.log(numbers);
+
+// Finding elements
+
+const arrNum = [1, 2, 3, 4, 1, 5]
+
+console.log(arrNum.indexOf(1));
+console.log(arrNum.lastIndexOf(1));
+console.log(arrNum.indexOf(1) !== -1);
+console.log(arrNum.includes(1));
+
+
+const courses = [
+    { id: 1, nameIs: 'Prasanna' },
+    { id: 2, nameIs: 'Rajan' },
+]
+
+const course = courses.find(courses => courses.nameIs === 'Prasanna')
+console.log(course);
+
+const numberIsArr = [1,2,3,4,5]
+
+// Remove the end of the array
+const last = numberIsArr.pop();
+console.log(numberIsArr);
+
+const numberIsArrOne = [1,2,3,4,5]
+// Remove the Beginning of the array
+const first = numberIsArrOne.shift();
+console.log(numberIsArrOne);
+
+const numberIsArrTwo = [1,2,3,4,5]
+// Remove the Middle of the array
+numberIsArrTwo.splice(2,2)
+console.log(numberIsArrTwo);
+
+
+let EmptArr = [1,2,3,4,5]
+let AsignArr = EmptArr
+// solution 1
+EmptArr = []
+
+// solution 2
+EmptArr.length = 0
+
+// solution 3
+EmptArr.splice(0,EmptArr.length)
+
+console.log(EmptArr);
+console.log(AsignArr);
+
+const firstNumber = [1,2,3]
+const SecondNumber = [4,5,6]
+
+const combined = firstNumber.concat(SecondNumber)
+console.log('combined',combined);
+
+const spreadArrOne = [4,5,6]
+const spreadArrTwo = [7,8,9]
+
+const UsingSpreadArr = [...spreadArrOne,'a',...spreadArrTwo,'b']
+const CombineSpreadArr = {...UsingSpreadArr}
+
+console.log(CombineSpreadArr);
+
+const numbersOne = [1,2,3]
+
+for(let numbers of numbersOne)
+        console.log(numbers);
+        
+numbersOne.forEach((numbers,index) => console.log(index,numbers))
